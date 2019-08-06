0f3377e98e7978b6559f421480efba0cdd4dba79
Christopher Torres <torrinkrisl@gmail.com>
2019-08-06 13:08:56 -0600

    08062019

--- client/src/components/Footer/Footer.js
+++ client/src/components/Footer/Footer.js
@@ -5,8 +5,8 @@ function Footer() {
     return (
         <footer className="footer">
             <p><i className="fab fa-react react-icon"></i></p>
-            <p>Evan E J Tucker</p>
-            <p><a id="githib" href="https://github.com/evanejtucker/react-auth-simple" target="blank"><i className="fab fa-github gh-icon"></i></a></p>
+            <p>Christopher, Holt, Miles, Anna</p>
+            <p><a id="githib" href="https://github.com/ChrisTorres47/cookbookProject3" target="blank"><i className="fab fa-github gh-icon"></i></a></p>
         </footer>
     )
 }
