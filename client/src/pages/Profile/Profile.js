043f88f8634d708d7c05beb0a0a4bd088dc7b2f0
Christopher Torres <torrinkrisl@gmail.com>
2019-08-07 14:49:46 -0600

    just done for the day

--- client/src/pages/Profile/Profile.js
+++ client/src/pages/Profile/Profile.js
@@ -1,4 +1,4 @@
-import React, {Component} from "react";
+import React, { Component } from "react";
 import "./Profile.scss";
 import { Button } from "reactstrap";
 import { Link } from "react-router-dom"
@@ -31,33 +31,66 @@ class Profile extends Component {
     }
 
     loading() {
-        setTimeout(()=> {
+        setTimeout(() => {
             this.setState({
                 loading: false
             })
-        }, 1000)  
+        }, 1000)
+    }
+    
+    saveRecipe(){
+
     }
 
     render() {
         return (
-            <div className="profilePage">
-                {this.state.loggedIn ? (
-                    <div className="profileBox">
-                        <h1 id="userTitle">Welcome {this.state.user.username}</h1>
-                    </div>
-                ) : (
-                    <div className="noUser">
-                        {!this.state.loading ? (
-                            <>
-                                <h1>please log in</h1>
-                                <Link className="loginLink" to="/login"><Button className="loginBtn" color="info" block>Login</Button></Link>
-                            </>
-                        ) : (
-                            <img id="loadingIcon" src="./assets/images/loading.gif" alt="loading"/>
+            <>
+                <div className="profilePage">
+                    {this.state.loggedIn ? (
+                        <div className="profileBox">
+                            <h1 id="userTitle">Welcome {this.state.user.username}</h1>
+                        </div>
+                    ) : (
+                            <div className="noUser">
+                                {!this.state.loading ? (
+                                    <>
+                                        <h1>please log in</h1>
+                                        <Link className="loginLink" to="/login"><Button className="loginBtn" color="info" block>Login</Button></Link>
+                                    </>
+                                ) : (
+                                        <img id="loadingIcon" src="./assets/images/loading.gif" alt="loading" />
+                                    )}
+                            </div>
                         )}
-                    </div> 
-                )}
-            </div>
+                </div>
+                <div class="container backColor text-light">
+                    <form>
+                        <fieldset>
+                            <legend>Add your recipe</legend>
+                            <div class="form-group">
+                                <label for="recipeName">Recipe name</label>
+                                <textarea class="form-control" id="recipeName" rows="1"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="ingredients">List your ingredients here</label>
+                                <textarea class="form-control" id="ingredients" rows="3"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="directions">List your directions here</label>
+                                <textarea class="form-control" id="directions" rows="3"></textarea>
+                            </div>
+                            <div class="form-group">
+                                <label for="recipeImage">Picture of your deliciousness</label>
+                                <textarea class="form-control" id="recipeImage" rows="1" placeholder="Enter a URL for an image of your recipe"></textarea>
+                            </div>
+                            <button type="submit" class="btn btn-primary rounded-pill" id="create" onClick="saveRecipe">Submit</button>
+                            <br /><br />
+
+                        </fieldset>
+                    </form>
+
+                </div>
+            </>
         )
     }
 }
