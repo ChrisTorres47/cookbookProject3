791d29e07f8d7c9076e723d94b06fb161a2738e4
Christopher Torres <torrinkrisl@gmail.com>
2019-08-12 13:55:54 -0600

    merging

--- client/src/components/Jumbotron/index.js
+++ client/src/components/Jumbotron/index.js
@@ -1,12 +1,4 @@
 import React from "react";
-
-function Jumbotron({ children }) {
-  return (
-    <div
-      style={{ height: 300, clear: "both", paddingTop: 120, textAlign: "center" }}
-      className="jumbotron"
-    >
-      {children}
 import "./style.css";
 import MyCarousel from "../Carousel/index";
 
