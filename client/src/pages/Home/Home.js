c60652aec10b7f35ce7b4183492c2714fb38aa45
Christopher Torres <torrinkrisl@gmail.com>
2019-08-13 12:10:14 -0600

    Addded some nice styling touches

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -66,7 +66,7 @@ class Home extends Component {
             </Row>
             <Row>
               <Col size="xs-12">
-                {!this.state.recipes.length ? (<h1 className="text-center">No Recipes to Display</h1>) : (
+                {!this.state.recipes.length ? (<h1 className="text-center"></h1>) : (
                   <ApiRecipeList>
                     {this.state.recipes.map(recipe => {
                       return (
